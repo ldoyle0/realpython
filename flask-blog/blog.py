0533b5fc87b28f4f1fa7b613248570da8e355071
ldoyle0 <ldoyle0@gmail.com>
2014-11-04 02:47:32 +0000

    filled out the blog app

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -4,14 +4,43 @@ import sqlite3
 
 #configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask("__name__")
 
+#checks for UPPERCASE variables
 app.config.from_object(__name__)
 
 #function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+#login page    
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+#logout page
+@app.route('/logout')
+def logout():
+    """docstring for logout"""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+#main page
+@app.route("/main")
+def main():
+    return render_template('main.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/hello")
+@app.route("/test")
+def search():
+    return "Hello"
 
 def hello_world():
     return "Hello, World!"
