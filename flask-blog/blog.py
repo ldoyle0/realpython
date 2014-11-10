b88fca3cc3458644738661dc3edfa43a3e82502b
ldoyle0 <ldoyle0@gmail.com>
2014-11-10 02:00:35 +0000

    we in there

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from functools import wraps
 
 import sqlite3
 
@@ -16,6 +17,17 @@ app.config.from_object(__name__)
 #function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+#login required
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 #login page    
 @app.route("/", methods=['GET', 'POST'])
@@ -39,6 +51,7 @@ def logout():
 
 #main page
 @app.route("/main")
+@login_required
 def main():
     return render_template('main.html')
     
