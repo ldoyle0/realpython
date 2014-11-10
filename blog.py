d92379524aa1f6050d1c7ed67d0c1b58a7200cc2
ldoyle0 <ldoyle0@gmail.com>
2014-11-10 02:57:59 +0000

    added Procfile

--- blog.py
+++ blog.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 from functools import wraps
-import os
 
 import sqlite3
 
@@ -57,5 +56,4 @@ def main():
     return render_template('main.html')
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run()
